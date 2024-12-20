dd6358345bd1b088672085364902beaa9fcf930d
Joe Fernandez <joefernandez@users.noreply.github.com>
2024-12-20 16:21:27 +0530

    feat: update Next.js configuration for enhanced performance and strict mode
    - Enabled reactStrictMode for improved error handling and debugging.
    - Added compression to optimize performance.
    - Introduced experimental CSS optimizations: optimizeCss, cssChunking, and inlineCss for better loading efficiency.
    - Disabled the poweredByHeader for enhanced security.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
+  compress: true,
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    cssChunking: true,
+    inlineCss: true,
+  },
+  poweredByHeader: false,
   images: {
     remotePatterns: [
       {
